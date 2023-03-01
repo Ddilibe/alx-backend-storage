f12010a72008289f564b2dbefb98e7c0356fa768
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-03-01 14:30:25 +0100

    [Chore]: Done with task 3
    Storing Lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -27,6 +27,30 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """ Function for storing the history of inputs and outputs of a
+        function
+        Args:
+            :params: method[Callable] - A callable function
+        Return:
+            This function returns a callable
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs) -> Callable:
+        """ Function for the actual counting
+            Args:
+                :params: args[List] - List of probable parameters
+                :params: kwargs[Dict] - Dict of probable parameters
+            Return: Returns a callable
+        """
+        key = method.__qualname__
+        self._redis.rpush("{}:inputs".format(key), str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush("{}:outputs".format(key), output)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
 class Cache(object):
     """docstring for Cache"""
     def __init__(self):
@@ -34,6 +58,7 @@ class Cache(object):
         self._redis = redis.Redis()
         self.flushdb = self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Any) -> str:
         """ A method for storing a key value pair in the redis server """
